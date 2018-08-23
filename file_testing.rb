e107cfa0189eedcffbe35ce3b93ed98021445e72
Henry Chu <chu.henry@hotmail.com>
2018-08-22 20:03:43 -0700

    hello

---
+++ file_testing.rb
@@ -0,0 +1,18 @@
+# File.foreach("test_file.txt") do |line|
+  # puts line unless line == "\n"
+  # print File.readlines("test_file.txt")
+
+# end
+# File.open("test_file.txt", "a") do |f|
+#   f.puts 'Abra Kadabra'
+# end
+
+# fi.each do |line|
+#   if line != "\n"
+#     puts "Line #{fi.lineno} " + "is: " + line
+#   end
+# end
+
+puts ARGV
+puts ARGV[0]
+puts 'work?'
